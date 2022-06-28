9a2edd6e005d3d3633cba2a92603186870c6543f
Sapik <Sapik_19@mail.ru>
2022-06-28 15:05:35 +0300

    Redesigned dynamic flashcards using 'Rest'

--- js/script.js
+++ js/script.js
@@ -139,25 +139,33 @@ window.addEventListener('DOMContentLoaded', function() {
     window.addEventListener('scroll', showModalByScroll);
 
     class MenuCard{
-        constructor(img, title, descr, price, parent){
+        constructor(img, title, descr, price, parent, ...classes){
             this.img = img;
             this.title = title;
             this.descr = descr;
             this.price = price;
             this.parent = document.querySelector(parent);
+            this.classes = classes;
         }
 
         createCard() {
             const element = document.createElement('div');
-            element.innerHTML = `<div class="menu__item">
-                <img src="${this.img}" alt="vegy">
-                <h3 class="menu__item-subtitle"></h3>
-                <div class="menu__item-descr">${this.descr}!</div>
-                <div class="menu__item-divider"></div>
-                <div class="menu__item-price">
-                    <div class="menu__item-cost">Цена:</div>
-                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                </div>
+
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
+            element.innerHTML = `
+            <img src="${this.img}" alt="vegy">
+            <h3 class="menu__item-subtitle"></h3>
+            <div class="menu__item-descr">${this.descr}!</div>
+            <div class="menu__item-divider"></div>
+            <div class="menu__item-price">
+                <div class="menu__item-cost">Цена:</div>
+                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
             </div>`;
             this.parent.append(element);
         }
@@ -186,5 +194,5 @@ window.addEventListener('DOMContentLoaded', function() {
         "430",
         ".menu .container"
     ).createCard();
-
+    
 });
